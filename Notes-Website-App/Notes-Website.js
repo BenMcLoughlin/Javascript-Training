f50c461a339e962854ee7fe32e2f9b44a822801c
BenMcLoughlin <benmcl@shaw.ca>
2019-01-02 08:47:38 -0800

    56. Rendering our filtered Data

--- Notes-Website-App/Notes-Website.js
+++ Notes-Website-App/Notes-Website.js
@@ -9,6 +9,26 @@ notes = [{
 	body: "get a new seat"
 }]
 
+const filters = {
+	searchText: ""
+}
+
+const renderNotes = function (notes, filters) {
+	const filteredNotes = notes.filter(function (note) {
+		return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+	})
+
+	document.querySelector('#notes-div').innerHTML = "";
+
+	filteredNotes.forEach(function (note) {
+		const noteEl = document.createElement("p")
+		noteEl.textContent = note.title
+		document.querySelector('#notes-div').appendChild(noteEl)
+	})
+}
+
+renderNotes(notes, filters)
+
 document.querySelector("#create-note").addEventListener("click", function (e) {
 e.target.textContent = "the button was clicked"
 })
@@ -18,5 +38,6 @@ document.querySelector("#remove-all").addEventListener('click', function () {
 	})
 })
 document.querySelector('#search-text').addEventListener("input", function (e) {
-	console.log(e.target.value);
+	filters.searchText = e.target.value
+	renderNotes (notes, filters)
 })
